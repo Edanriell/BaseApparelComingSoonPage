8c8ad8bea5fa4deb6c35bf9205a79b1c493866e1
Edanriell <Edanriell@gmail.com>
2024-07-31 23:26:52 +0300

    finished mobile version of section

--- Client/pages/home/ui.tsx
+++ Client/pages/home/ui.tsx
@@ -6,7 +6,71 @@ export const HomePage: FC = () => {
 	return (
 		<MainLayout>
 			<section>
-				<h1>Home Page</h1>
+				<picture>
+					<source
+						type="image/webp"
+						media="(min-width: 1440px)"
+						srcSet="/images/raster/woman-image-desktop@1x.webp 1x, /images/raster/woman-image-desktop@2x.webp 2x"
+					/>
+					<source
+						type="image/webp"
+						media="(min-width: 375px)"
+						srcSet="/images/raster/woman-image-mobile@1x.webp 1x, /images/raster/woman-image-mobile@2x.webp 2x"
+					/>
+					<source
+						type="image/jpg"
+						media="(min-width: 1440px)"
+						srcSet="/images/raster/woman-image-desktop@1x.jpg 1x, /images/raster/woman-image-desktop@2x.jpg 2x"
+					/>
+					<source
+						type="image/jpg"
+						media="(min-width: 375px)"
+						srcSet="/images/raster/woman-image-mobile@1x.jpg 1x, /images/raster/woman-image-mobile@2x.jpg 2x"
+					/>
+					<img
+						className="w-full h-[25rem] object-cover"
+						src="/images/raster/woman-image-desktop@1x.jpg"
+						srcSet="/images/raster/woman-image-desktop@2x.jpg"
+						alt="Woman"
+					/>
+				</picture>
+				<div className="mt-[6.4rem] mb-[9.2rem] px-[3.2rem]">
+					<h1 className="font-josefinSans font-light text-[4rem] leading-[105%] tracking-[0.27em] text-center text-oriental-pink-400 uppercase mb-[1.6rem]">
+						We&apos;re{" "}
+						<strong className="font-josefinSans font-semibold text-[4rem] leading-[105%] tracking-[0.27em] text-center text-masala-900 uppercase">
+							coming soon
+						</strong>
+					</h1>
+					<p className="font-josefinSans font-normal text-[1.4rem] leading-[157%] text-center text-oriental-pink-400 mb-[3.2rem]">
+						Hello fellow shoppers! We&apos;re currently building our new fashion store. Add your
+						email below to stay up-to-date with announcements and our launch deals.
+					</p>
+					<form className="relative" method="POST">
+						<div className="relative">
+							<label className="sr-only" htmlFor="customer-email">
+								Customer email address
+							</label>
+							<input
+								className="w-full max-h-[4.8rem] font-josefinSans font-normal text-[1.4rem] leading-[200%] text-masala-900 placeholder:text-oriental-pink-400-05 pt-[1.1rem] pb-[0.9rem] pl-[2.4rem] pr-[6.4rem] bg-transparent rounded-[2.8rem] border border-solid border-oriental-pink-400"
+								id="customer-email"
+								name="customer-email"
+								type="email"
+								placeholder="Email Address"
+							/>
+							<button
+								className="px-[2.8rem] py-[1.6rem] max-h-[4.8rem] rounded-[2.8rem] shadow-buttonShadow bg-buttonGradient absolute top-0 right-0"
+								type="submit"
+							>
+								<img
+									className="w-[0.884rem] h-[1.767rem]"
+									src="/images/vector/icons/arrow-right.svg"
+									alt=""
+								/>
+								<span className="sr-only">Send data</span>
+							</button>
+						</div>
+					</form>
+				</div>
 			</section>
 		</MainLayout>
 	);
--- Client/tailwind.config.ts
+++ Client/tailwind.config.ts
@@ -24,7 +24,46 @@ export default {
 			extrabold: "800",
 			black: "900"
 		},
-		extend: {}
+		fontFamily: {
+			josefinSans: ["Josefin Sans"]
+		},
+		extend: {
+			colors: {
+				"oriental-pink": {
+					"50": "#faf6f6",
+					"100": "#f5ebeb",
+					"200": "#eedada",
+					"300": "#e2bfbf",
+					"400": "#ce9898",
+					"400-05": "rgba(206, 152, 152, 0.5)",
+					"500": "#bb7878",
+					"600": "#a55d5d",
+					"700": "#894c4c",
+					"800": "#734141",
+					"900": "#613b3b",
+					"950": "#331c1c"
+				},
+				masala: {
+					"50": "#f4f3f2",
+					"100": "#e3e1de",
+					"200": "#c9c3bf",
+					"300": "#aaa09a",
+					"400": "#92847d",
+					"500": "#83756f",
+					"600": "#70615e",
+					"700": "#5b4e4d",
+					"800": "#4f4544",
+					"900": "#423a3a",
+					"950": "#272121"
+				}
+			},
+			boxShadow: {
+				buttonShadow: "0 1.5rem 2rem 0 rgba(198, 110, 110, 0.25)"
+			},
+			backgroundImage: {
+				buttonGradient: "linear-gradient(135deg, #f8bfbf 0%, #ee8b8b 100%)"
+			}
+		}
 	},
 	plugins: []
 } satisfies Config;
