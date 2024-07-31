dc2a4951228e3d1a09b87fd257eb5c96b9e8f3be
Edanriell <Edanriell@gmail.com>
2024-07-31 12:20:48 +0300

    finished

--- Client/app/routes/_index.tsx
+++ Client/app/routes/_index.tsx
@@ -1,45 +1,16 @@
 import type { MetaFunction } from "@remix-run/node";
 
+import { HomePage } from "@pages/home";
+
 export const meta: MetaFunction = () => {
-	return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
+	return [
+		{ title: "Coming soon" },
+		{
+			name: "description",
+			content:
+				"Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals."
+		}
+	];
 };
 
-export default function Index() {
-	return (
-		<div className="font-sans p-4">
-			<h1 className="text-3xl">Welcome to Remix</h1>
-			<ul className="list-disc mt-4 pl-6 space-y-2">
-				<li>
-					<a
-						className="text-blue-700 underline visited:text-purple-900"
-						target="_blank"
-						href="https://remix.run/start/quickstart"
-						rel="noreferrer"
-					>
-						5m Quick Start
-					</a>
-				</li>
-				<li>
-					<a
-						className="text-blue-700 underline visited:text-purple-900"
-						target="_blank"
-						href="https://remix.run/start/tutorial"
-						rel="noreferrer"
-					>
-						30m Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						className="text-blue-700 underline visited:text-purple-900"
-						target="_blank"
-						href="https://remix.run/docs"
-						rel="noreferrer"
-					>
-						Remix Docs
-					</a>
-				</li>
-			</ul>
-		</div>
-	);
-}
+export default HomePage;
