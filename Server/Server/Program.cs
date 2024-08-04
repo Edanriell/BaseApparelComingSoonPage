f2a8570e016b73fe63ea0d725c13e09d2624477e
Edanriell <Edanriell@gmail.com>
2024-08-04 14:36:03 +0300

    Almost finished  server

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,13 +1,28 @@
+using System.Reflection;
+using Server.Data;
+using Server.Interfaces;
+using Server.Repositories;
+using Server.Routing;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowLocalhost",
+		policy =>
+		{
+			policy.WithOrigins("https://localhost:7109", "http://localhost:7109").AllowAnyMethod().AllowAnyHeader();
+		});
+});
+
+builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddScoped<IUserEmailsRepository, UserEmailsRepository>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
@@ -16,29 +31,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-				{
-					"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-				};
+app.UseCors("AllowLocalhost");
 
-app.MapGet("/weatherforecast", () =>
-	{
-		var forecast = Enumerable.Range(1, 5).Select(index =>
-				new WeatherForecast
-					(
-						DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-						Random.Shared.Next(-20, 55),
-						summaries[Random.Shared.Next(summaries.Length)]
-					))
-		   .ToArray();
-		return forecast;
-	})
-   .WithName("GetWeatherForecast")
-   .WithOpenApi();
+app.MapEndpoints(Assembly.GetExecutingAssembly());
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
